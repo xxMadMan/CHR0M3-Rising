f78a9c3e3e41db8757182ecbd790debb59dcae41
CuddlyAssassin <bryanthesnowman@hotmail.com>
2017-11-03 17:16:56 +1030

    Turret rotates on the Y axis to pan around to look at you

--- Assets/Scripts/AI/TurretAI.cs
+++ Assets/Scripts/AI/TurretAI.cs
@@ -4,55 +4,17 @@ using UnityEngine;
 
 public class TurretAI : MonoBehaviour {
 
-    //public Transform target, aim, head;
-    //public float reloadTime = 1f, turnSpeed = 5f, firePauseTime = 0.25f, range = 3f;
-    //public Transform[] muzzlePos;
-    //public bool canSee;
-
-
-    public Transform player;
+    public Transform player, body, head;
+    public float damping;
 
     void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            var lookDir = player.position - transform.position;
-            lookDir.y = 0;
-            transform.rotation = Quaternion.LookRotation(lookDir);
+            var lookPos = player.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
         }
     }
-
-    //   float nextFireTime, nextMoveTime;
-
-    //void Start ()
-    //   {
-
-    //}
-
-
-    //void Update ()
-    //   {
-    //	if (target)
-    //       {
-    //           if (Time.time >= nextMoveTime)
-    //           {
-    //               aim.LookAt(target);
-    //               aim.eulerAngles = new Vector3(0, aim.eulerAngles.y, 0);
-    //               head.rotation = Quaternion.Lerp(head.rotation, aim.rotation, Time.deltaTime * turnSpeed);
-    //           }
-    //           if (Time.time >= nextFireTime && canSee == true) 
-    //           {
-    //               Fire();
-    //           }
-    //       }
-
-    //}
-
-    //   void Fire()
-    //   {
-    //       nextFireTime = Time.time + reloadTime;
-    //       nextMoveTime = Time.time + firePauseTime;
-    //   }
-
-
 }

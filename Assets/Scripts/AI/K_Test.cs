44a8b329adf66b7aefb73f58cd8230482ee29df2
xxMadMan <kalebismadman@gmail.com>
2017-11-03 18:06:26 +1030

    Added K_Test for turrets

---
+++ Assets/Scripts/AI/K_Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_Test : MonoBehaviour {
+
+    public float playerDistance;
+    public float aimingDistance;
+    public Transform _player;
+    public float damping = 3f;
+
+    public GameObject head, body;
+
+    void FixedUpdate()
+    {
+        playerDistance = Vector3.Distance(_player.position, transform.position);
+        if (playerDistance < aimingDistance)
+        {
+            BodyRotation();
+            Aiming();
+        }
+    }
+
+    void BodyRotation()
+    {
+        var lookPos = _player.position - body.transform.position;
+        lookPos.y = 0;
+        var rotationY = Quaternion.LookRotation(lookPos);
+        body.transform.rotation = Quaternion.Slerp(body.transform.rotation, rotationY, Time.deltaTime * damping);
+    }
+
+    void Aiming()
+    {
+        Quaternion rotation = Quaternion.LookRotation(_player.position - head.transform.position);
+        head.transform.rotation = Quaternion.Slerp(head.transform.rotation, rotation, Time.deltaTime * damping);
+    }
+}
--- Assets/Scripts/AI/TurretAI.cs
+++ Assets/Scripts/AI/TurretAI.cs
@@ -17,11 +17,11 @@ public class TurretAI : MonoBehaviour {
             var rotationY = Quaternion.LookRotation(lookPos);
             body.transform.rotation = Quaternion.Slerp(body.transform.rotation, rotationY, Time.deltaTime * damping);
 
-            //the head needs to do it, but on the x axis.
-            var lookDir = player.position - head.transform.position;
-            lookDir.x = 0;
-            var rotationX = Quaternion.LookRotation(lookDir);
-            head.transform.rotation = Quaternion.Slerp(head.transform.rotation, rotationX, Time.deltaTime * damping);
+            ////the head needs to do it, but on the x axis.
+            //var lookDir = player.position - head.transform.position;
+            //lookDir.x = 0;
+            //var rotationX = Quaternion.LookRotation(lookDir);
+            //head.transform.rotation = Quaternion.Slerp(head.transform.rotation, rotationX, Time.deltaTime * damping);
         }
     }
 }

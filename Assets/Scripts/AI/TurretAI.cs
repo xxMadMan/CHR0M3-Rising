b02075c11ce9ff0b625d2553eddd7879f0c0203d
CuddlyAssassin <bryanthesnowman@hotmail.com>
2017-11-03 17:53:43 +1030

    Pushing turretAI

--- Assets/Scripts/AI/TurretAI.cs
+++ Assets/Scripts/AI/TurretAI.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class TurretAI : MonoBehaviour {
 
     public Transform player, body, head;
-    public float damping;
+    public float damping = 3f;
 
     void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            var lookPos = player.position - transform.position;
+            //the body needs to do this
+            var lookPos = player.position - body.transform.position;
             lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            var rotationY = Quaternion.LookRotation(lookPos);
+            body.transform.rotation = Quaternion.Slerp(body.transform.rotation, rotationY, Time.deltaTime * damping);
+
+            //the head needs to do it, but on the x axis.
+            var lookDir = player.position - head.transform.position;
+            lookDir.x = 0;
+            var rotationX = Quaternion.LookRotation(lookDir);
+            head.transform.rotation = Quaternion.Slerp(head.transform.rotation, rotationX, Time.deltaTime * damping);
         }
     }
 }
